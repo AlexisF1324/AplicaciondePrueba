7e7b01b9eb996052b7fecbf51e44c194f314f65b
Santo Tomas <alexisf1324gmail.com>
2023-08-21 20:35:08 -0400

    Subida2

---
+++ app/src/main/java/com/example/aplicaciondeprueba/VerCompras.java
@@ -0,0 +1,13 @@
+package com.example.aplicaciondeprueba;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VerCompras extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compra);
+    }
+}
